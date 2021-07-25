2da7e3b7a1c74975cbc67260da3ae2ad4793fcf3
Bliss Singhal <54075580+bluelilly1@users.noreply.github.com>
2021-07-25 15:20:04 -0700

    Create lesson4.java

---
+++ lesson4.java
@@ -0,0 +1,18 @@
+public class Main {
+//change the name of the class to anything else
+
+//create at least two attributes of that class
+
+
+//create at least three methods that use or modifies these attributes
+
+
+
+  public static void main(String[] args) {
+   //make two objects
+
+   //assign attributes to those objects
+
+   //call methods on those objects
+  }
+}
